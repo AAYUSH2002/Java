625f1a7f7dab203d6d12901a863f80b6856190b2
Aayush Yadav <57173240+AAYUSH2002@users.noreply.github.com>
2022-02-24 12:24:58 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,9 @@
+public class Pattern {
+    public static void main(String[] arg){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        
+    }
+}
